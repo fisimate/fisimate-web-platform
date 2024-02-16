785843cb75a0eb61a18497346caf47a1e21816e3
dafariski77 <rskdafa@gmail.com>
2024-02-17 04:52:44 +0700

    feat(auth): add logic for login

---
+++ src/app/auth/login/page.jsx
@@ -0,0 +1,69 @@
+"use client";
+import { useLogin } from "@/hooks/useAth";
+import { useFormik } from "formik";
+import { Button, useToast } from "@chakra-ui/react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+  const { push } = useRouter();
+  const toast = useToast();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: () => {
+      mutate(formik.values);
+    },
+  });
+
+  const handleChange = (e) => {
+    formik.setFieldValue(e.target.name, e.target.value);
+  };
+
+  const { mutate, isPending } = useLogin({
+    onSuccess: (response) => {
+      const result = response?.data?.data;
+
+      Cookies.set("token", JSON.stringify(result));
+
+      return push("/dashboard");
+    },
+    onError: (error) => {
+      const result = error.response.data;
+
+      toast({
+        title: result.message,
+        status: "error",
+        isClosable: true,
+        position: "top-right",
+      });
+    },
+  });
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <p>{isPending}</p>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          placeholder="Your Email"
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          placeholder="Your Password"
+        />
+
+        <Button type="submit" isLoading={isPending} disabled={isPending}>
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
