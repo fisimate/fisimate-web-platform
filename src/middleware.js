9dcd699299616fa5e2e72afedbcf1f97ddc5420d
dafariski77 <rskdafa@gmail.com>
2024-05-05 01:29:16 +0700

    fix(auth): still cant fix login middleware

--- src/middleware.js
+++ src/middleware.js
@@ -5,15 +5,11 @@ export function middleware(request) {
   const token = request.cookies.get("token");
   const isLoggedIn = checkIsLoggedIn(token);
 
-  if (request.nextUrl.pathname.startsWith("/auth/login") && isLoggedIn) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
-
   if (!isLoggedIn) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 }
 
 export const config = {
-  matcher: ["/((?!api|_next).*)"],
+  matcher: ["/((?!api|_next|auth/login).*)"],
 };
