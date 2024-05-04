b3d55484eb7dad10050252b9e25eb6ca29d7cbc8
dafariski77 <rskdafa@gmail.com>
2024-05-04 19:00:04 +0700

    perf: check when user has logged in

--- src/middleware.js
+++ src/middleware.js
@@ -5,11 +5,15 @@ export function middleware(request) {
   const token = request.cookies.get("token");
   const isLoggedIn = checkIsLoggedIn(token);
 
+  if (request.nextUrl.pathname.startsWith("/auth/login") && isLoggedIn) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
   if (!isLoggedIn) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|auth/login).*)"],
+  matcher: ["/((?!api|_next).*)"],
 };
