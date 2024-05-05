c4291e0210d11094ca4423f5f8e1bfae37b85594
dafariski77 <rskdafa@gmail.com>
2024-05-05 14:29:29 +0700

    style: init basic layout

---
+++ src/app/(home)/banks/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Banks | Fisimate",
+  description: "Fisimate web app",
+};
+
+export default function RootLayout({ children }) {
+  return children;
+}
---
+++ src/app/(home)/profiles/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Profile | Fisimate",
+  description: "Fisimate web app",
+};
+
+export default function RootLayout({ children }) {
+  return children;
+}
