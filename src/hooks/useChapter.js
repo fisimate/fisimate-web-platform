dde8f4639f151f263f29ada3af302808c8823861
dafariski77 <rskdafa@gmail.com>
2024-05-05 21:54:46 +0700

    fix(hooks): change querykey to array

--- src/hooks/useChapter.js
+++ src/hooks/useChapter.js
@@ -3,7 +3,7 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useChapterQuery = (token) => {
   return useQuery({
-    queryKey: "chapters",
+    queryKey: ["chapters"],
     queryFn: async () => {
       return await axiosInstance.get("/chapters", {
         headers: {
