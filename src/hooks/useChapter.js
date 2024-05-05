b01d84f82100c76e234da01e1c1630ab7a918e00
dafariski77 <rskdafa@gmail.com>
2024-05-05 21:48:07 +0700

    fix(hooks): add token on headers

--- src/hooks/useChapter.js
+++ src/hooks/useChapter.js
@@ -1,19 +1,27 @@
 import axiosInstance from "@/libs/axios";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
-export const useChapterQuery = () => {
+export const useChapterQuery = (token) => {
   return useQuery({
     queryKey: "chapters",
     queryFn: async () => {
-      return await axiosInstance.get("/chapters");
+      return await axiosInstance.get("/chapters", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
     },
   });
 };
 
-export const useChapterMutation = ({ onSuccess, onError }) => {
+export const useChapterMutation = ({ onSuccess, onError, token }) => {
   return useMutation({
     mutationFn: async (body) => {
-      return await axiosInstance.post("/chapters", body);
+      return await axiosInstance.post("/chapters", body, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
     },
     onSuccess,
     onError,
