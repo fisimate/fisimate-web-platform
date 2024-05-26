ec37b7d328610666356c0574c12c64625ea40357
dafariski77 <rskdafa@gmail.com>
2024-05-26 19:56:48 +0700

    style: add CardStat component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,42 +1,7 @@
-import {
-  Card,
-  CardBody,
-  HStack,
-  IconButton,
-  Stat,
-  StatLabel,
-  StatNumber,
-} from "@chakra-ui/react";
+import React from 'react'
 
-export default function CustomCard({ label, value, icon }) {
+export default function CustomCard() {
   return (
-    <Card
-      overflow={"hidden"}
-      variant={"unstyled"}
-      rounded={"xl"}
-      w={"lg"}
-      px={6}
-      py={4}
-    >
-      <CardBody>
-        <HStack spacing={6}>
-          <IconButton
-            isRound={true}
-            size={"lg"}
-            colorScheme={"blue"}
-            fontSize="24px"
-            isActive={false}
-            cursor={"default"}
-            icon={icon}
-          />
-          <Stat>
-            <StatLabel color={"grey"}>{label}</StatLabel>
-            <StatNumber fontWeight={"bold"} mt={0}>
-              {value}
-            </StatNumber>
-          </Stat>
-        </HStack>
-      </CardBody>
-    </Card>
-  );
+    <div>CustomCard</div>
+  )
 }
