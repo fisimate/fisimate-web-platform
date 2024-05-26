812e7f7db4d8e925890589d5b074e7d84b6bdb58
dafariski77 <rskdafa@gmail.com>
2024-05-26 23:11:25 +0700

    style(dashboard): change some padding and margin

--- src/app/(home)/dashboard/page.jsx
+++ src/app/(home)/dashboard/page.jsx
@@ -1,23 +1,21 @@
 "use client";
 import CardStat from "@/components/CardStat";
 import BarChart from "@/components/Chart/BarChart";
-import LineChart from "@/components/Chart/LineChart";
-import { useAuthStore } from "@/stores/app-store";
 import { Flex, Select } from "@chakra-ui/react";
 import { FiBook, FiDatabase, FiMonitor, FiUsers } from "react-icons/fi";
 
 export default function Dashboard() {
   return (
     <div className="">
-      <Flex direction={"row"} justify={"space-between"} gap={8}>
+      <Flex direction={"row"} justify={"space-between"} gap={8} mt={6}>
         <CardStat label={"Total Students"} value={12} icon={<FiUsers />} />
         <CardStat label={"Banks"} value={24} icon={<FiDatabase />} />
         <CardStat label={"Materials"} value={42} icon={<FiBook />} />
         <CardStat label={"Simulations"} value={90} icon={<FiMonitor />} />
       </Flex>
-      <div className="mt-8 flex justify-between gap-8">
+      <div className="mt-6 flex justify-between gap-8">
         <div className="bg-white p-8 w-3/5 rounded-xl">
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between">
             <h2 className="font-semibold">Total Student Taking the Quiz</h2>
             <Select w={"fit-content"}>
               <option value="">Per Month</option>
