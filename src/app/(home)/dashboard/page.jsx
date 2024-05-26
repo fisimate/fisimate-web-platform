0a0a7ca06881d3093adbfb5fe597316cc1fdacd0
dafariski77 <rskdafa@gmail.com>
2024-05-26 20:20:48 +0700

    feat: add basic style for dashboard

--- src/app/(home)/dashboard/page.jsx
+++ src/app/(home)/dashboard/page.jsx
@@ -1,28 +1,77 @@
 "use client";
-import CustomCard from "@/components/Card";
+import CardStat from "@/components/CardStat";
+import BarChart from "@/components/Chart/BarChart";
+import LineChart from "@/components/Chart/LineChart";
 import { useAuthStore } from "@/stores/app-store";
-import { Flex } from "@chakra-ui/react";
-import {
-  FiBook,
-  FiDatabase,
-  FiFileText,
-  FiMonitor,
-  FiUsers,
-} from "react-icons/fi";
+import { Flex, Select } from "@chakra-ui/react";
+import { FiBook, FiDatabase, FiMonitor, FiUsers } from "react-icons/fi";
 
 export default function Dashboard() {
-  const { user } = useAuthStore();
-
   return (
     <div className="">
       <Flex direction={"row"} justify={"space-between"} gap={8}>
-        <CustomCard label={"Students"} value={12} icon={<FiUsers />} />
-        <CustomCard label={"Banks"} value={12} icon={<FiDatabase />} />
-        <CustomCard label={"Materials"} value={12} icon={<FiBook />} />
-        <CustomCard label={"Simulations"} value={12} icon={<FiMonitor />} />
-        <CustomCard label={"Exams"} value={12} icon={<FiFileText />} />
+        <CardStat label={"Total Students"} value={12} icon={<FiUsers />} />
+        <CardStat label={"Banks"} value={24} icon={<FiDatabase />} />
+        <CardStat label={"Materials"} value={42} icon={<FiBook />} />
+        <CardStat label={"Simulations"} value={90} icon={<FiMonitor />} />
       </Flex>
-      <h1>{JSON.stringify(user)}</h1>
+      <div className="mt-8 flex justify-between gap-8">
+        <div className="bg-white p-8 w-3/5 rounded-xl">
+          <div className="flex justify-between items-center">
+            <h2 className="font-semibold">Total Student Taking the Quiz</h2>
+            <Select w={"fit-content"}>
+              <option value="">Per Month</option>
+              <option value="">Per Day</option>
+            </Select>
+          </div>
+          <div className="w-full h-full flex items-center">
+            <div className="w-full">
+              <BarChart />
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-8 w-2/5 rounded-xl">
+          <h2 className="font-semibold">Top Student</h2>
+          <div className="flex flex-col p-2 mt-5 gap-6">
+            <div className="bg-blue-500 p-4 rounded-full flex flex-row justify-between items-center">
+              <div className="flex flex-row gap-5 items-center">
+                <div className="bg-white h-16 w-16 rounded-full"></div>
+                <p className="text-white font-medium text-lg">Tsaqif Akrom</p>
+              </div>
+              <div className="bg-gray-300 px-4 py-2 rounded-full">
+                <p className="font-extrabold">400</p>
+              </div>
+            </div>
+            <div className="bg-blue-500 p-4 rounded-full flex flex-row justify-between items-center">
+              <div className="flex flex-row gap-5 items-center">
+                <div className="bg-white h-16 w-16 rounded-full"></div>
+                <p className="text-white font-medium text-lg">Tsaqif Akrom</p>
+              </div>
+              <div className="bg-gray-300 px-4 py-2 rounded-full">
+                <p className="font-extrabold">400</p>
+              </div>
+            </div>
+            <div className="bg-blue-500 p-4 rounded-full flex flex-row justify-between items-center">
+              <div className="flex flex-row gap-5 items-center">
+                <div className="bg-white h-16 w-16 rounded-full"></div>
+                <p className="text-white font-medium text-lg">Tsaqif Akrom</p>
+              </div>
+              <div className="bg-gray-300 px-4 py-2 rounded-full">
+                <p className="font-extrabold">400</p>
+              </div>
+            </div>
+            <div className="bg-blue-500 p-4 rounded-full flex flex-row justify-between items-center">
+              <div className="flex flex-row gap-5 items-center">
+                <div className="bg-white h-16 w-16 rounded-full"></div>
+                <p className="text-white font-medium text-lg">Tsaqif Akrom</p>
+              </div>
+              <div className="bg-gray-300 px-4 py-2 rounded-full">
+                <p className="font-extrabold">400</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
