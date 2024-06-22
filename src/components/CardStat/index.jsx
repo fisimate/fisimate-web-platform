7c5a12a02d9e02cd312c05c32b4c813352a1c591
dafariski77 <rskdafa@gmail.com>
2024-06-23 04:23:28 +0700

    style: add dashboard card stat component

---
+++ src/components/CardStat/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function CardStat({ title, total, icon }) {
+  return (
+    <div className="rounded-md border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="flex text-primary h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4 text-2xl">
+        {icon}
+      </div>
+
+      <div className="mt-4 flex items-end justify-between">
+        <div>
+          <h4 className="text-title-md font-bold text-black dark:text-white">
+            {total}
+          </h4>
+          <span className="text-sm font-medium">{title}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
