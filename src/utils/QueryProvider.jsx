d462290f924c2ba75ba6a8614441c3304b7fb09d
dafariski77 <rskdafa@gmail.com>
2024-06-23 21:34:23 +0700

    chore: set tanstack query retry to 1

--- src/utils/QueryProvider.jsx
+++ src/utils/QueryProvider.jsx
@@ -1,9 +1,18 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const InitProvider = ({ children }) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
