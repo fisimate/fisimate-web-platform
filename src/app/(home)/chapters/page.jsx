987669595278344af1a674d065dd0da54aaa9108
dafariski77 <rskdafa@gmail.com>
2024-06-27 20:16:25 +0700

    feat(chapter): add basic layout for display chapters data

--- src/app/(home)/chapters/page.jsx
+++ src/app/(home)/chapters/page.jsx
@@ -1,19 +1,125 @@
 "use client";
+import Alert from "@/components/Alert";
+import Breadcrumb from "@/components/Breadcrumb";
+import Button from "@/components/Button";
+import Modal from "@/components/Modal";
+import Table from "@/components/Table";
+import TableAction from "@/components/Table/TableAction";
 import { useGetChapters } from "@/hooks/useChapter";
 import { useGetToken } from "@/hooks/useToken";
-import { Spinner } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
 
 export default function ExamBank() {
-  const token = useGetToken();
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
+  const [showAlert, setShowAlert] = useState({
+    isShow: false,
+    title: "",
+    message: "",
+    type: "error",
+  });
 
-  const { data, isLoading } = useGetChapters({ token });
+  const data = [
+    {
+      id: "62987cb2-3067-4598-98c4-f518d186beb0",
+      name: "Keseimbangan Benda",
+      slug: "keseimbangan-benda",
+    },
+    {
+      id: "0ad20750-f303-46c1-93c2-f5264fb954e3",
+      name: "Keseimbangan Benda",
+      slug: "keseimbangan-benda",
+    },
+  ];
+
+  const { push } = useRouter();
+
+  const openModal = (modalData) => {
+    setSelectedData(modalData);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => setModalOpen(false);
+
+  // const token = useGetToken();
+
+  // const { data, isLoading } = useGetChapters({ token });
+
+  const fields = ["name", "slug"];
+
+  const headers = [
+    {
+      title: "Name",
+    },
+    {
+      title: "Slug",
+    },
+  ];
+
+  const actions = (actionData) => (
+    <>
+      <TableAction icon={<FiEdit />} action={() => push("/")} />
+      <TableAction icon={<FiTrash2 />} action={() => openModal(actionData)} />
+    </>
+  );
+
+  const modalActions = [
+    {
+      label: "Cancel",
+      onClick: closeModal,
+      primary: false,
+    },
+    {
+      label: "Delete",
+      onClick: () => {
+        if (selectedData) {
+          // mutate({ token, userId: selectedUser.id });
+
+          closeModal();
+        }
+      },
+      primary: true,
+    },
+  ];
 
   return (
-    <div className="">
-      {isLoading && <Spinner />}
-      {data?.data?.data.map((item, i) => {
-        return <p key={i}>{JSON.stringify(item)}</p>;
-      })}
-    </div>
+    <React.Fragment>
+      <Breadcrumb pageName={"Data Bab"} />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title={"Delete data"}
+        content={"Are you sure want to delete this data?"}
+        actions={modalActions}
+      />
+      <div className="flex flex-col gap-10">
+        {showAlert.isShow && (
+          <Alert
+            type={"error"}
+            message={showAlert.message}
+            title={showAlert.title}
+          />
+        )}
+        <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+          <div className="flex justify-end mb-6">
+            <Link href={"/banks/chapters/create"}>
+              <Button text={"Create Bab"} />
+            </Link>
+          </div>
+          <Table
+            headers={headers}
+            // data={data?.data?.data}
+            data={data}
+            action={actions}
+            fields={fields}
+            // isLoading={isLoading}
+            // isRefetching={isRefetching}
+          />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
