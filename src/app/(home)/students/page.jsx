460ee655a8fd2df2f6c969a151dd38f8fcdfc014
dafariski77 <rskdafa@gmail.com>
2024-07-15 21:47:46 +0700

    feat: add student page

---
+++ src/app/(home)/students/page.jsx
@@ -0,0 +1,134 @@
+"use client";
+import Breadcrumb from "@/components/Breadcrumb";
+import Button from "@/components/Button";
+import Modal from "@/components/Modal";
+import Table from "@/components/Table";
+import TableAction from "@/components/Table/TableAction";
+import { useDeleteStudent, useGetAllStudents } from "@/hooks/useStudent";
+import { useGetToken } from "@/hooks/useToken";
+import { useToast } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+
+export default function StudentPage() {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
+
+  const { push } = useRouter();
+  const toast = useToast();
+
+  const openModal = (modalData) => {
+    setSelectedData(modalData);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => setModalOpen(false);
+
+  const token = useGetToken();
+
+  const { data, isLoading, isRefetching, refetch } = useGetAllStudents({
+    token,
+  });
+
+  const { mutate } = useDeleteStudent({
+    token,
+    onError: (error) => {
+      const result = error.response.data;
+
+      toast({
+        title: result.message,
+        status: "error",
+        isClosable: true,
+        position: "top-right",
+      });
+    },
+    onSuccess: () => {
+      toast({
+        title: "Berhasil hapus data!",
+        status: "success",
+        isClosable: true,
+        position: "top-right",
+      });
+
+      refetch();
+    },
+  });
+
+  const fields = ["profilePicture", "fullname", "nis", "email"];
+
+  const headers = [
+    {
+      title: "Avatar",
+    },
+    {
+      title: "Fullname",
+    },
+    {
+      title: "NIS",
+    },
+    {
+      title: "Email",
+    },
+  ];
+
+  const actions = (actionData) => (
+    <>
+      <TableAction
+        icon={<FiEdit />}
+        action={() => push(`/students/${actionData.id}`)}
+      />
+      <TableAction icon={<FiTrash2 />} action={() => openModal(actionData)} />
+    </>
+  );
+
+  const modalActions = [
+    {
+      label: "Cancel",
+      onClick: closeModal,
+      primary: false,
+    },
+    {
+      label: "Delete",
+      onClick: () => {
+        if (selectedData) {
+          mutate({ token, studentId: selectedData.id });
+
+          closeModal();
+        }
+      },
+      primary: true,
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      <Breadcrumb pageName={"Siswa"} />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title={"Delete data"}
+        content={"Are you sure want to delete this data?"}
+        actions={modalActions}
+      />
+      <div className="flex flex-col gap-10">
+        <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+          <div className="flex justify-end mb-6">
+            <Link href={"/students/create"}>
+              <Button text={"Create Siswa"} />
+            </Link>
+          </div>
+          <Table
+            headers={headers}
+            data={data?.data?.data}
+            action={actions}
+            fields={fields}
+            isLoading={isLoading}
+            isRefetching={isRefetching}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
