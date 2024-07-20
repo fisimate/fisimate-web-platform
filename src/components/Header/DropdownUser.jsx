b48337d2f4f32be57d723cfa99e61ea27df5375c
dafariski77 <rskdafa@gmail.com>
2024-07-21 03:08:52 +0700

    chore: change link to button on dropdown

--- src/components/Header/DropdownUser.jsx
+++ src/components/Header/DropdownUser.jsx
@@ -56,11 +56,10 @@ const DropdownUser = () => {
 
   return (
     <div className="relative">
-      <Link
+      <button
         ref={trigger}
         onClick={() => setDropdownOpen(!dropdownOpen)}
         className="flex items-center gap-4"
-        href="#"
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
@@ -100,7 +99,7 @@ const DropdownUser = () => {
             fill=""
           />
         </svg>
-      </Link>
+      </button>
 
       {/* <!-- Dropdown Start --> */}
       <div
