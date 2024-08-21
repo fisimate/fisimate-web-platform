3912a80c6a1694c2d2bb16129a7a0984a6949180
dafariski77 <rskdafa@gmail.com>
2024-08-22 04:48:52 +0700

    fix: fixing boolean

--- src/app/(home)/simulations/[simulationId]/page.jsx
+++ src/app/(home)/simulations/[simulationId]/page.jsx
@@ -339,12 +339,16 @@ export default function SimulationDetail({ params }) {
     onSubmit: (values) => {
       const formData = new FormData();
       formData.append("text", values.text);
-
-      values.options.forEach((option, index) => {
-        formData.append(`options[${index}][id]`, option.id ?? "");
-        formData.append(`options[${index}][text]`, option.text);
-        formData.append(`options[${index}][isCorrect]`, option.isCorrect);
-      });
+      formData.append(
+        "options",
+        JSON.stringify(
+          values.options.map((option) => ({
+            id: option.id ?? null,
+            text: option.text,
+            isCorrect: option.isCorrect,
+          }))
+        )
+      );
 
       if (files != null) {
         if (!files.type) {
